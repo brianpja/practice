219bc3f9b44bd7c13e7319dba13d684c6c413105
Brian James <brianpja@gmail.com>
2017-12-08 15:00:32 -0800

    queueTime

--- practice.js
+++ practice.js
@@ -12,3 +12,17 @@ function bouncingBall(h, bounce, window) {
 // console.log(bouncingBall(3, .66, 1.5));
 // console.log(bouncingBall(30, .66, 1.5));
 // console.log(bouncingBall(30, 0.9999999999, 35))
+
+
+function queueTime(customers, n) {
+  for (let i = 0; i < n; i++)
+}
+
+console.log(queueTime([5,3,4], 1))
+//5
+console.log(queueTime([10,2,3,3], 2))
+//10
+console.log(queueTime([2,3,10], 2))
+//12
+console.log(queueTime([1,2,3,4,5], 100))
+//5
