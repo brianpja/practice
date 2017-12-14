c109bd7d2fe7f50079b032930332e9a06ce599b7
Brian James <brianpja@gmail.com>
2017-12-13 16:39:54 -0800

    binary search

--- coordinates.js
+++ coordinates.js
@@ -14,6 +14,65 @@ function coordinates(array, int) {
 }
 
 
-console.log(coordinates([[0,1], [10,12]], 10))
-console.log(coordinates([[-3,-1,1], [4,9,13], [15,18,19]], 18))
-console.log(coordinates([[-3,-1,1], [4,9,13], [15,18,19]], 24))
+// console.log(coordinates([[0,1], [10,12]], 10))
+// console.log(coordinates([[-3,-1,1], [4,9,13], [15,18,19]], 18))
+// console.log(coordinates([[-3,-1,1], [4,9,13], [15,18,19]], 24))
+
+
+// function binarySearch(array, int) {
+//   let midVal;
+//   let x;
+//   let y;
+//
+//   if (array.length %2 === 1) {
+//     x = Math.floor(array.length / 2);
+//     y = Math.floor(array.length / 2);
+//     midVal = array[x][y];
+//     console.log(midVal);
+//     if (midVal === int) {
+//       return [x,y];
+//     }
+//   }
+//   if (array.length %2 === 0) {
+//     x = array.length / 2;
+//     y = 0;
+//     midVal = array[x][y];
+//     console.log(midVal);
+//     if (midVal === int) {
+//       return [x,y]
+//     }
+//   }
+// }
+//
+// console.log(binarySearch([[0,1], [10,12]], 10))
+// console.log(binarySearch([[-3,-1,1], [4,9,13], [15,18,19]], 18))
+// console.log(binarySearch([[-3,-1,1], [4,9,13], [15,18,19]], 24))
+
+
+
+function find(array, val) {
+
+  const x = outerSearch(array, val);
+
+  if (x) return x;
+
+}
+
+function outerSearch(array, val, left = 0, right = array.length - 1) {
+  const midpoint = Math.floor((right - left)/2);
+  console.log(midpoint)
+  const innerArray = array[midpoint];
+  if (innerArray[0] <= val && val <= innerArray[innerArray.length - 1]) {
+    return midpoint;
+  }
+  else if (val < innerArray[0]){
+    outerSearch(array, val, left, midpoint);
+  }
+  else if (val > innerArray[])
+}
+
+
+
+console.log(find([[0,1], [10,12]], 10))
+console.log(find([[-3,-1,1], [4,9,13], [15,18,19]], 18))
+console.log(find([[-3,-1,1], [4,9,13], [15,18,19]], 24))
