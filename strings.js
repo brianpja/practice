59639db38494b9691577c5a1a9a13b4def998a13
Brian James <brianpja@gmail.com>
2018-03-29 13:50:15 -0700

    strings

--- strings.js
+++ strings.js
@@ -12,3 +12,21 @@ function caps(str) {
 
 caps('galvanize web development immersive')
 caps('WDI resident role');
+
+//takes out adjacent characters if they are the same
+//and then checks the whole string again
+function super_reduced_string(s){
+
+    for (let i = 0; i < s.length - 1; i++) {
+        if (s[i] === s[i + 1]) {
+            s = s.slice(0, i) + s.slice(i+2);
+            i = -1;
+        }
+    }
+    if (s.length) {
+        return s;
+    }
+    else {
+        return 'Empty String'
+    }
+}
